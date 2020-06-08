f936c374999a7d607905e64bef9bb6685457c7d9
Gerald.Shen <gerald.shen@mail.utoronto.ca>
2020-06-07 23:20:41 -0400

    Pushing roll a ball project and gitignore

---
+++ roll a ball/Assets/src/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.count = 0;
+        rb = GetComponent<Rigidbody>();
+        countText.text = "Count: " + count.ToString();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, movevertical);
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickupObject"))
+        {
+            other.gameObject.SetActive(false);
+            this.count++;
+            countText.text = "Count: " + count.ToString();
+            if (count== 6)
+            {
+                this.winText.text = "you win";
+            }
+        }
+    }
+}
